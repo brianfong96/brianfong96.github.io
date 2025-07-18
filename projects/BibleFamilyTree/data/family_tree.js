6ac42aea1d7c05a8bafbb75fbb1961b9899b6c69
Brian Fong <brianwkfong@gmail.com>
2025-07-18 13:55:35 -0700

    Expand Bible family tree data and enforce node spacing

--- projects/BibleFamilyTree/data/family_tree.js
+++ projects/BibleFamilyTree/data/family_tree.js
@@ -3,18 +3,32 @@ window.familyTreeData = [
     "id": "adam-001",
     "name": "Adam",
     "aliases": [],
-    "children": ["cain-001", "abel-001", "seth-002"],
+    "children": [
+      "cain-001",
+      "abel-001",
+      "seth-002"
+    ],
     "parents": [],
-    "references": ["Genesis 2:7", "Genesis 5:1-5"],
+    "references": [
+      "Genesis 2:7",
+      "Genesis 5:1-5"
+    ],
     "notes": "First man created by God"
   },
   {
     "id": "eve-001",
     "name": "Eve",
     "aliases": [],
-    "children": ["cain-001", "abel-001", "seth-002"],
+    "children": [
+      "cain-001",
+      "abel-001",
+      "seth-002"
+    ],
     "parents": [],
-    "references": ["Genesis 2:22", "Genesis 3:20"],
+    "references": [
+      "Genesis 2:22",
+      "Genesis 3:20"
+    ],
     "notes": "First woman created by God"
   },
   {
@@ -22,31 +36,1074 @@ window.familyTreeData = [
     "name": "Cain",
     "aliases": [],
     "children": [],
-    "parents": ["adam-001", "eve-001"],
-    "references": ["Genesis 4:1-16"]
+    "parents": [
+      "adam-001",
+      "eve-001"
+    ],
+    "references": [
+      "Genesis 4:1-16"
+    ]
   },
   {
     "id": "abel-001",
     "name": "Abel",
     "aliases": [],
     "children": [],
-    "parents": ["adam-001", "eve-001"],
-    "references": ["Genesis 4:2-8"]
+    "parents": [
+      "adam-001",
+      "eve-001"
+    ],
+    "references": [
+      "Genesis 4:2-8"
+    ]
   },
   {
     "id": "seth-002",
     "name": "Seth",
     "aliases": [],
-    "children": ["enosh-001"],
-    "parents": ["adam-001", "eve-001"],
-    "references": ["Genesis 4:25", "Genesis 5:3-8"]
+    "children": [
+      "enosh-001"
+    ],
+    "parents": [
+      "adam-001",
+      "eve-001"
+    ],
+    "references": [
+      "Genesis 4:25",
+      "Genesis 5:3-8"
+    ]
   },
   {
     "id": "enosh-001",
     "name": "Enosh",
     "aliases": [],
+    "children": [
+      "kenan-001"
+    ],
+    "parents": [
+      "seth-002"
+    ],
+    "references": [
+      "Genesis 5:6-11"
+    ]
+  },
+  {
+    "id": "kenan-001",
+    "name": "Kenan",
+    "aliases": [],
+    "children": [
+      "mahalalel-001"
+    ],
+    "parents": [
+      "enosh-001"
+    ],
+    "references": [
+      "Genesis 5:9-14"
+    ]
+  },
+  {
+    "id": "mahalalel-001",
+    "name": "Mahalalel",
+    "aliases": [],
+    "children": [
+      "jared-001"
+    ],
+    "parents": [
+      "kenan-001"
+    ],
+    "references": [
+      "Genesis 5:12-17"
+    ]
+  },
+  {
+    "id": "jared-001",
+    "name": "Jared",
+    "aliases": [],
+    "children": [
+      "enoch-001"
+    ],
+    "parents": [
+      "mahalalel-001"
+    ],
+    "references": [
+      "Genesis 5:15-20"
+    ]
+  },
+  {
+    "id": "enoch-001",
+    "name": "Enoch",
+    "aliases": [],
+    "children": [
+      "methuselah-001"
+    ],
+    "parents": [
+      "jared-001"
+    ],
+    "references": [
+      "Genesis 5:21-24"
+    ]
+  },
+  {
+    "id": "methuselah-001",
+    "name": "Methuselah",
+    "aliases": [],
+    "children": [
+      "lamech-001"
+    ],
+    "parents": [
+      "enoch-001"
+    ],
+    "references": [
+      "Genesis 5:25-27"
+    ]
+  },
+  {
+    "id": "lamech-001",
+    "name": "Lamech",
+    "aliases": [],
+    "children": [
+      "noah-001"
+    ],
+    "parents": [
+      "methuselah-001"
+    ],
+    "references": [
+      "Genesis 5:28-31"
+    ]
+  },
+  {
+    "id": "noah-001",
+    "name": "Noah",
+    "aliases": [],
+    "children": [
+      "shem-001",
+      "ham-001",
+      "japheth-001"
+    ],
+    "parents": [
+      "lamech-001"
+    ],
+    "references": [
+      "Genesis 5:32",
+      "Genesis 6:9"
+    ]
+  },
+  {
+    "id": "shem-001",
+    "name": "Shem",
+    "aliases": [],
+    "children": [
+      "arphaxad-001"
+    ],
+    "parents": [
+      "noah-001"
+    ],
+    "references": [
+      "Genesis 5:32",
+      "Genesis 11:10"
+    ]
+  },
+  {
+    "id": "ham-001",
+    "name": "Ham",
+    "aliases": [],
     "children": [],
-    "parents": ["seth-002"],
-    "references": ["Genesis 4:26", "Genesis 5:6-11"]
+    "parents": [
+      "noah-001"
+    ],
+    "references": [
+      "Genesis 5:32"
+    ]
+  },
+  {
+    "id": "japheth-001",
+    "name": "Japheth",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "noah-001"
+    ],
+    "references": [
+      "Genesis 5:32"
+    ]
+  },
+  {
+    "id": "arphaxad-001",
+    "name": "Arphaxad",
+    "aliases": [],
+    "children": [
+      "shelah-001"
+    ],
+    "parents": [
+      "shem-001"
+    ],
+    "references": [
+      "Genesis 11:10-13"
+    ]
+  },
+  {
+    "id": "shelah-001",
+    "name": "Shelah",
+    "aliases": [],
+    "children": [
+      "eber-001"
+    ],
+    "parents": [
+      "arphaxad-001"
+    ],
+    "references": [
+      "Genesis 11:12-15"
+    ]
+  },
+  {
+    "id": "eber-001",
+    "name": "Eber",
+    "aliases": [],
+    "children": [
+      "peleg-001"
+    ],
+    "parents": [
+      "shelah-001"
+    ],
+    "references": [
+      "Genesis 11:14-17"
+    ]
+  },
+  {
+    "id": "peleg-001",
+    "name": "Peleg",
+    "aliases": [],
+    "children": [
+      "reu-001"
+    ],
+    "parents": [
+      "eber-001"
+    ],
+    "references": [
+      "Genesis 11:16-19"
+    ]
+  },
+  {
+    "id": "reu-001",
+    "name": "Reu",
+    "aliases": [],
+    "children": [
+      "serug-001"
+    ],
+    "parents": [
+      "peleg-001"
+    ],
+    "references": [
+      "Genesis 11:18-21"
+    ]
+  },
+  {
+    "id": "serug-001",
+    "name": "Serug",
+    "aliases": [],
+    "children": [
+      "nahor-001"
+    ],
+    "parents": [
+      "reu-001"
+    ],
+    "references": [
+      "Genesis 11:20-23"
+    ]
+  },
+  {
+    "id": "nahor-001",
+    "name": "Nahor",
+    "aliases": [],
+    "children": [
+      "terah-001"
+    ],
+    "parents": [
+      "serug-001"
+    ],
+    "references": [
+      "Genesis 11:22-25"
+    ]
+  },
+  {
+    "id": "terah-001",
+    "name": "Terah",
+    "aliases": [],
+    "children": [
+      "abraham-001"
+    ],
+    "parents": [
+      "nahor-001"
+    ],
+    "references": [
+      "Genesis 11:24-26"
+    ]
+  },
+  {
+    "id": "abraham-001",
+    "name": "Abraham",
+    "aliases": [
+      "Abram"
+    ],
+    "children": [
+      "isaac-001",
+      "ishmael-001"
+    ],
+    "parents": [
+      "terah-001"
+    ],
+    "references": [
+      "Genesis 11:26",
+      "Genesis 17:5"
+    ]
+  },
+  {
+    "id": "ishmael-001",
+    "name": "Ishmael",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "abraham-001"
+    ],
+    "references": [
+      "Genesis 16:11"
+    ]
+  },
+  {
+    "id": "isaac-001",
+    "name": "Isaac",
+    "aliases": [],
+    "children": [
+      "jacob-001",
+      "esau-001"
+    ],
+    "parents": [
+      "abraham-001"
+    ],
+    "references": [
+      "Genesis 21:3",
+      "Genesis 25:19-26"
+    ]
+  },
+  {
+    "id": "esau-001",
+    "name": "Esau",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "isaac-001"
+    ],
+    "references": [
+      "Genesis 25:25"
+    ]
+  },
+  {
+    "id": "jacob-001",
+    "name": "Jacob",
+    "aliases": [
+      "Israel"
+    ],
+    "children": [
+      "reuben-001",
+      "simeon-001",
+      "levi-001",
+      "judah-001",
+      "dan-001",
+      "naphtali-001",
+      "gad-001",
+      "asher-001",
+      "issachar-001",
+      "zebulun-001",
+      "joseph-001",
+      "benjamin-001"
+    ],
+    "parents": [
+      "isaac-001"
+    ],
+    "references": [
+      "Genesis 25:26",
+      "Genesis 35:23-26"
+    ]
+  },
+  {
+    "id": "reuben-001",
+    "name": "Reuben",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 29:32"
+    ]
+  },
+  {
+    "id": "simeon-001",
+    "name": "Simeon",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 29:33"
+    ]
+  },
+  {
+    "id": "levi-001",
+    "name": "Levi",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 29:34"
+    ]
+  },
+  {
+    "id": "judah-001",
+    "name": "Judah",
+    "aliases": [],
+    "children": [
+      "perez-001"
+    ],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 29:35"
+    ]
+  },
+  {
+    "id": "dan-001",
+    "name": "Dan",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:6"
+    ]
+  },
+  {
+    "id": "naphtali-001",
+    "name": "Naphtali",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:8"
+    ]
+  },
+  {
+    "id": "gad-001",
+    "name": "Gad",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:11"
+    ]
+  },
+  {
+    "id": "asher-001",
+    "name": "Asher",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:13"
+    ]
+  },
+  {
+    "id": "issachar-001",
+    "name": "Issachar",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:18"
+    ]
+  },
+  {
+    "id": "zebulun-001",
+    "name": "Zebulun",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:20"
+    ]
+  },
+  {
+    "id": "joseph-001",
+    "name": "Joseph",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 30:24"
+    ]
+  },
+  {
+    "id": "benjamin-001",
+    "name": "Benjamin",
+    "aliases": [],
+    "children": [],
+    "parents": [
+      "jacob-001"
+    ],
+    "references": [
+      "Genesis 35:18"
+    ]
+  },
+  {
+    "id": "perez-001",
+    "name": "Perez",
+    "aliases": [],
+    "children": [
+      "hezron-001"
+    ],
+    "parents": [
+      "judah-001"
+    ],
+    "references": [
+      "Genesis 38:29"
+    ]
+  },
+  {
+    "id": "hezron-001",
+    "name": "Hezron",
+    "aliases": [],
+    "children": [
+      "ram-001"
+    ],
+    "parents": [
+      "perez-001"
+    ],
+    "references": [
+      "Ruth 4:18"
+    ]
+  },
+  {
+    "id": "ram-001",
+    "name": "Ram",
+    "aliases": [],
+    "children": [
+      "amminadab-001"
+    ],
+    "parents": [
+      "hezron-001"
+    ],
+    "references": [
+      "Ruth 4:19"
+    ]
+  },
+  {
+    "id": "amminadab-001",
+    "name": "Amminadab",
+    "aliases": [],
+    "children": [
+      "nahshon-001"
+    ],
+    "parents": [
+      "ram-001"
+    ],
+    "references": [
+      "Ruth 4:19"
+    ]
+  },
+  {
+    "id": "nahshon-001",
+    "name": "Nahshon",
+    "aliases": [],
+    "children": [
+      "salmon-001"
+    ],
+    "parents": [
+      "amminadab-001"
+    ],
+    "references": [
+      "Ruth 4:20"
+    ]
+  },
+  {
+    "id": "salmon-001",
+    "name": "Salmon",
+    "aliases": [],
+    "children": [
+      "boaz-001"
+    ],
+    "parents": [
+      "nahshon-001"
+    ],
+    "references": [
+      "Ruth 4:21"
+    ]
+  },
+  {
+    "id": "boaz-001",
+    "name": "Boaz",
+    "aliases": [],
+    "children": [
+      "obed-001"
+    ],
+    "parents": [
+      "salmon-001"
+    ],
+    "references": [
+      "Ruth 4:21"
+    ]
+  },
+  {
+    "id": "obed-001",
+    "name": "Obed",
+    "aliases": [],
+    "children": [
+      "jesse-001"
+    ],
+    "parents": [
+      "boaz-001"
+    ],
+    "references": [
+      "Ruth 4:22"
+    ]
+  },
+  {
+    "id": "jesse-001",
+    "name": "Jesse",
+    "aliases": [],
+    "children": [
+      "david-001"
+    ],
+    "parents": [
+      "obed-001"
+    ],
+    "references": [
+      "Ruth 4:22"
+    ]
+  },
+  {
+    "id": "david-001",
+    "name": "David",
+    "aliases": [],
+    "children": [
+      "solomon-001"
+    ],
+    "parents": [
+      "jesse-001"
+    ],
+    "references": [
+      "1 Samuel 16:13"
+    ]
+  },
+  {
+    "id": "solomon-001",
+    "name": "Solomon",
+    "aliases": [],
+    "children": [
+      "rehoboam-001"
+    ],
+    "parents": [
+      "david-001"
+    ],
+    "references": [
+      "2 Samuel 12:24"
+    ]
+  },
+  {
+    "id": "rehoboam-001",
+    "name": "Rehoboam",
+    "aliases": [],
+    "children": [
+      "abijah-001"
+    ],
+    "parents": [
+      "solomon-001"
+    ],
+    "references": [
+      "1 Kings 11:43"
+    ]
+  },
+  {
+    "id": "abijah-001",
+    "name": "Abijah",
+    "aliases": [],
+    "children": [
+      "asa-001"
+    ],
+    "parents": [
+      "rehoboam-001"
+    ],
+    "references": [
+      "1 Kings 14:31"
+    ]
+  },
+  {
+    "id": "asa-001",
+    "name": "Asa",
+    "aliases": [],
+    "children": [
+      "jehoshaphat-001"
+    ],
+    "parents": [
+      "abijah-001"
+    ],
+    "references": [
+      "1 Kings 15:8"
+    ]
+  },
+  {
+    "id": "jehoshaphat-001",
+    "name": "Jehoshaphat",
+    "aliases": [],
+    "children": [
+      "joram-001"
+    ],
+    "parents": [
+      "asa-001"
+    ],
+    "references": [
+      "1 Kings 15:24"
+    ]
+  },
+  {
+    "id": "joram-001",
+    "name": "Joram",
+    "aliases": [],
+    "children": [
+      "uzziah-001"
+    ],
+    "parents": [
+      "jehoshaphat-001"
+    ],
+    "references": [
+      "2 Kings 8:16"
+    ]
+  },
+  {
+    "id": "uzziah-001",
+    "name": "Uzziah",
+    "aliases": [],
+    "children": [
+      "jotham-001"
+    ],
+    "parents": [
+      "joram-001"
+    ],
+    "references": [
+      "2 Kings 15:13"
+    ]
+  },
+  {
+    "id": "jotham-001",
+    "name": "Jotham",
+    "aliases": [],
+    "children": [
+      "ahaz-001"
+    ],
+    "parents": [
+      "uzziah-001"
+    ],
+    "references": [
+      "2 Kings 15:32"
+    ]
+  },
+  {
+    "id": "ahaz-001",
+    "name": "Ahaz",
+    "aliases": [],
+    "children": [
+      "hezekiah-001"
+    ],
+    "parents": [
+      "jotham-001"
+    ],
+    "references": [
+      "2 Kings 15:38"
+    ]
+  },
+  {
+    "id": "hezekiah-001",
+    "name": "Hezekiah",
+    "aliases": [],
+    "children": [
+      "manasseh-001"
+    ],
+    "parents": [
+      "ahaz-001"
+    ],
+    "references": [
+      "2 Kings 18:1"
+    ]
+  },
+  {
+    "id": "manasseh-001",
+    "name": "Manasseh",
+    "aliases": [],
+    "children": [
+      "amon-001"
+    ],
+    "parents": [
+      "hezekiah-001"
+    ],
+    "references": [
+      "2 Kings 20:21"
+    ]
+  },
+  {
+    "id": "amon-001",
+    "name": "Amon",
+    "aliases": [],
+    "children": [
+      "josiah-001"
+    ],
+    "parents": [
+      "manasseh-001"
+    ],
+    "references": [
+      "2 Kings 21:18"
+    ]
+  },
+  {
+    "id": "josiah-001",
+    "name": "Josiah",
+    "aliases": [],
+    "children": [
+      "jeconiah-001"
+    ],
+    "parents": [
+      "amon-001"
+    ],
+    "references": [
+      "2 Kings 21:24"
+    ]
+  },
+  {
+    "id": "jeconiah-001",
+    "name": "Jeconiah",
+    "aliases": [
+      "Jehoiachin"
+    ],
+    "children": [
+      "shealtiel-001"
+    ],
+    "parents": [
+      "josiah-001"
+    ],
+    "references": [
+      "2 Kings 24:6"
+    ]
+  },
+  {
+    "id": "shealtiel-001",
+    "name": "Shealtiel",
+    "aliases": [],
+    "children": [
+      "zerubbabel-001"
+    ],
+    "parents": [
+      "jeconiah-001"
+    ],
+    "references": [
+      "Ezra 3:2"
+    ]
+  },
+  {
+    "id": "zerubbabel-001",
+    "name": "Zerubbabel",
+    "aliases": [],
+    "children": [
+      "abiud-001"
+    ],
+    "parents": [
+      "shealtiel-001"
+    ],
+    "references": [
+      "Ezra 3:2"
+    ]
+  },
+  {
+    "id": "abiud-001",
+    "name": "Abiud",
+    "aliases": [],
+    "children": [
+      "eliakim-001"
+    ],
+    "parents": [
+      "zerubbabel-001"
+    ],
+    "references": [
+      "Matthew 1:13"
+    ]
+  },
+  {
+    "id": "eliakim-001",
+    "name": "Eliakim",
+    "aliases": [],
+    "children": [
+      "azor-001"
+    ],
+    "parents": [
+      "abiud-001"
+    ],
+    "references": [
+      "Matthew 1:13"
+    ]
+  },
+  {
+    "id": "azor-001",
+    "name": "Azor",
+    "aliases": [],
+    "children": [
+      "zadok-001"
+    ],
+    "parents": [
+      "eliakim-001"
+    ],
+    "references": [
+      "Matthew 1:13"
+    ]
+  },
+  {
+    "id": "zadok-001",
+    "name": "Zadok",
+    "aliases": [],
+    "children": [
+      "akim-001"
+    ],
+    "parents": [
+      "azor-001"
+    ],
+    "references": [
+      "Matthew 1:14"
+    ]
+  },
+  {
+    "id": "akim-001",
+    "name": "Akim",
+    "aliases": [],
+    "children": [
+      "eliud-001"
+    ],
+    "parents": [
+      "zadok-001"
+    ],
+    "references": [
+      "Matthew 1:14"
+    ]
+  },
+  {
+    "id": "eliud-001",
+    "name": "Eliud",
+    "aliases": [],
+    "children": [
+      "eleazar-001"
+    ],
+    "parents": [
+      "akim-001"
+    ],
+    "references": [
+      "Matthew 1:15"
+    ]
+  },
+  {
+    "id": "eleazar-001",
+    "name": "Eleazar",
+    "aliases": [],
+    "children": [
+      "matthan-001"
+    ],
+    "parents": [
+      "eliud-001"
+    ],
+    "references": [
+      "Matthew 1:15"
+    ]
+  },
+  {
+    "id": "matthan-001",
+    "name": "Matthan",
+    "aliases": [],
+    "children": [
+      "jacob-002"
+    ],
+    "parents": [
+      "eleazar-001"
+    ],
+    "references": [
+      "Matthew 1:15"
+    ]
+  },
+  {
+    "id": "jacob-002",
+    "name": "Jacob",
+    "aliases": [],
+    "children": [
+      "joseph-002"
+    ],
+    "parents": [
+      "matthan-001"
+    ],
+    "references": [
+      "Matthew 1:16"
+    ]
+  },
+  {
+    "id": "joseph-002",
+    "name": "Joseph",
+    "aliases": [
+      "Joseph of Nazareth"
+    ],
+    "children": [
+      "jesus-001"
+    ],
+    "parents": [
+      "jacob-002"
+    ],
+    "references": [
+      "Matthew 1:16"
+    ]
+  },
+  {
+    "id": "mary-001",
+    "name": "Mary",
+    "aliases": [],
+    "children": [
+      "jesus-001"
+    ],
+    "parents": [],
+    "references": [
+      "Matthew 1:16"
+    ]
+  },
+  {
+    "id": "jesus-001",
+    "name": "Jesus",
+    "aliases": [
+      "Christ"
+    ],
+    "children": [],
+    "parents": [
+      "joseph-002",
+      "mary-001"
+    ],
+    "references": [
+      "Matthew 1:16"
+    ]
   }
 ];
